cc823d0f7d33bbfca2b0c725c2e6e8b813498902
Lauren B Wilner <wilner.lauren@gmail.com>
2024-04-23 08:11:38 -0700

    numerator calculation

--- 02_underlying_psps_clean.R
+++ 02_underlying_psps_clean.R
@@ -8,6 +8,8 @@
 library(pacman)
 p_load(sf, tigris, MetBrewer, lubridate, arrow, raster, tidyverse)
 
+CRS = 'EPSG:3310' # this is california albers
+
 raw_dir <- ("~/Desktop/Desktop/epidemiology_PhD/data/raw/psps_circuit_data/")
 raw_raster_dir <- ("~/Desktop/Desktop/epidemiology_PhD/data/raw/")
 clean_dir <- ("~/Desktop/Desktop/epidemiology_PhD/data/clean/")
@@ -27,7 +29,7 @@ businesses <- read_parquet(paste0(clean_dir, "us_zcta_cbp.parquet"))
 ca_pop_raster <- raster(paste0(raw_raster_dir, "CAPOP_2020_100m_TOTAL.tif"))
 
 zcta_shp <- st_read(paste0(clean_dir, "us_zcta_boundaries.geojson"))
-zcta_shp <- st_transform(zcta_shp, crs(ca_pop_raster)) # tbd if this is the right direction
+zcta_shp <- st_transform(zcta_shp, crs(CRS)) # tbd if this is the right direction
 
 #-------------------------------------------------
 # helper functions
@@ -132,24 +134,56 @@ write_parquet(psps_expanded, paste0(clean_dir, "us_circuit_psps_by_hr.parquet"))
     # b. get from polylines to pixels
     # c. get from pixels to zctas
 # read in file created in python 
-psps_zcta <- read_parquet(paste0(clean_dir, "us_circuit_psps_by_hr_zcta.parquet"))
-
-# step 3: merge households and businesses at zcta level
-
-pop <- households %>% 
-        merge(businesses, by = "zcta") %>%
-        mutate(pop = households + businesses) %>% 
-        dplyr::select(zcta, pop)
-# psps <- psps_zcta %>% 
-#     left_join(pop, by = c("zcta" = "zcta"))
-
-
+denom <- read_parquet(paste0(clean_dir, "ca_gridded_zcta_pop.parquet"))
+num <- read_parquet(paste0(clean_dir, "ca_zcta_customers_impacted.parquet"))
+psps_clean <- num %>% 
+            left_join(denom, by = "zcta")
 
 # step 4: estimate the number of hours in every 24 hour period where {x}% or more of the customers were without power.
-# we will use a data driven threshold (25%, 50%, etc.)
-# look at the distribution of percents and pick a reasonable cutpoint.
-# count up # of hours that have more than that % off
-# if we want a binary cut for a synthetic control, that can be based on the distribution
+    # we will use a data driven threshold (25%, 50%, etc.)
+    # look at the distribution of percents and pick a reasonable cutpoint.
+    # count up # of hours that have more than that % off
+    # if we want a binary cut for a synthetic control, that can be based on the distribution
+thresholds <- c(0.25, 0.5, 0.75)
+# for(threshold in thresholds){
+psps_clean <- psps_clean %>% 
+    mutate(customers_out_per_hr = customers_impacted/(as.numeric(row_end) - as.numeric(row_start)),
+        pct_cust_out = customers_out_per_hr/daily_pop,
+        pct_out_over_thresh = ifelse(pct_cust_out > threshold, 1, 0))
+
+# calculate the number of hours in every 24h period where {x}% or more of the customers were without power
+# psps_collapse <- psps_clean %>% 
+
+    # create histogram of pct_out value with vertical line at x% threshold using ggplot
+    ggplot(psps_clean, aes(x = pct_out_over_thresh)) + 
+        geom_histogram(bins = 100, fill = "light blue") + 
+        labs(title = paste("Histogram of percent of customers impacted (ZCTA level PSPS event-hour with", threshold*100, "% threshold"), 
+            x = "Percent customers impacted", 
+            y = "Frequency") + 
+        geom_vline(xintercept = threshold, color = "red", size = 2) + 
+        theme_bw()
+# }
+
+# map the biggest outages 
+psps_big <- psps_clean %>% 
+    group_by(psps_event_id) %>% 
+    mutate(total_customers_impacted_event = sum(total_customers_impacted)) %>% 
+    ungroup() %>%
+    arrange(desc(total_customers_impacted_event)) %>%
+    slice_head(n = 10)
+
+psps_big_gdf <- psps_big %>% 
+    left_join(zcta_shp, by = c("zcta" = "ZCTA5CE10")) %>%
+    st_as_sf(coords = c("longitude", "latitude"), crs = 4326) %>%
+    st_transform(crs = CRS)
+
+ggplot() + 
+    geom_sf(data = zcta_shp, fill = "grey") + 
+    geom_sf(data = psps_big_gdf, aes(fill = customers_out_per_hr), size = 0.5) + 
+    scale_fill_viridis_c() + 
+    labs(title = "Top 10 PSPS Events by Customers Impacted per Hour", 
+        fill = "Customers Impacted") + 
+    theme_bw()
 
 
 # Questions
@@ -159,15 +193,6 @@ pop <- households %>%
             # since each customer can only be on one segment so we should sum the customers
             # but if we want average duration then we should average the start/end times so that
             # duration still adds up
-    # QUESTION: circuit-event_id should be the same as sub_event_id, but it is not. there is 
-        # sometimes `circuit_event_id_X` and more than 1. so, i am grouping by sub_event_id,
-        # circuit_name_ica, and psps_event_id - but we should ask bethany next time we chat.
-        # not urgent for now.
-    # Comment: I am using the customers out per hr as the total_customers_out/duration for a
-        # given circuit-event-subevent and then creating a row for every hr and then propogating that var forward.
-        # there are also rows that are less than an hr if the outage was not precisely a whole number of hours of outage.
-    # Comment:
-        # total_customers_impacted is the sum of customers impacted in a given circuit-event-subevent,
-            # even though the rows are event-subevent-circuit-hours
-# Step 3: 
-    # QUESTION: which crs should i use for CA?
+# Step 4: 
+    # QUESTION: do we want to use a ceiling? if ratio of cust_out:pop is more than .8, we cap at .8? do we redistribute the customers? 
+    # QUESTION: i  am proportionally splitting customers_impacted by zcta, but not by time, which can create outliers if/when we do anything time based. this is because when i expand the data out to an hourly dataset, some rows are less than a full hr. for example, if an outage lasted 6h23m, then there will be 6 rows that are 1h in duration and one row that is 23min. this means that if there were 44 people impacted by this outage, we will have customers_impacted_per_hr at 44 for most rows (since its equal to customers_impacted when we have an hourly dataset) but then if we were to come into official rate space, it would be ~114. i would propose that we weight the customers_out in a row by the # of minutes (over 60) that that row represents. so that a row that is only 23m gets 16 people, rather than 44, in my example.ß
