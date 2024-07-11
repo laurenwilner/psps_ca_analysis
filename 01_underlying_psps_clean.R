dbd41a68f507a4ed719b842527207d35aea20753
Lauren B Wilner <wilner.lauren@gmail.com>
2024-07-11 15:46:02 -0700

    updates to send exposure data to tarik and caitlin

--- 01_underlying_psps_clean.R
+++ 01_underlying_psps_clean.R
@@ -6,7 +6,7 @@
 #-------------------------------------------------
 # setup
 library(pacman)
-p_load(sf, tigris, MetBrewer, lubridate, arrow, raster, tidyverse, scales)
+p_load(sf, tigris, MetBrewer, lubridate, arrow, raster, tidyverse, scales, knitr, kableExtra)
 
 CRS = 'EPSG:3310' # this is california albers
 
@@ -15,24 +15,6 @@ raw_raster_dir <- ("~/Desktop/Desktop/epidemiology_PhD/data/raw/")
 clean_dir <- ("~/Desktop/Desktop/epidemiology_PhD/data/clean/")
 plot_dir <- ("~/Desktop/Desktop/epidemiology_PhD/projects/psps/plots/")
 psps_file_name <- '2023.07.clean_psps_data.csv'
-
-#-------------------------------------------------
-# import
-psps_circuit <- read_csv(paste0(raw_dir, psps_file_name))
-
-pge_polyline <- st_read(paste0(raw_dir, "ICA_circuits/PGE/PGE_circuits_lines/PGE_circuits_lines.shp"))
-sce_polyline <- st_read(paste0(raw_dir, "ICA_circuits/SCE/SCE_circuits/SCE_circuits.shp"))
-sdge_polyline <- st_read(paste0(raw_dir, "ICA_circuits/SDGE_circuits/SDGE_circuits.shp"))
-
-households <- read_parquet(paste0(clean_dir, "us_zcta_households.parquet"))
-businesses <- read_parquet(paste0(clean_dir, "us_zcta_cbp.parquet"))
-
-ca_pop_raster <- raster(paste0(raw_raster_dir, "CAPOP_2020_100m_TOTAL.tif"))
-
-zcta_shp <- st_read(paste0(clean_dir, "us_zcta_boundaries.geojson"))
-zcta_shp <- st_transform(zcta_shp, crs(CRS)) # tbd if this is the right direction
-zcta_ca <- read_parquet(paste0(clean_dir, "us_ca_zcta_shp.parquet"))
-zcta_ca <- st_as_sf(zcta_ca, crs = CRS)
 
 #-------------------------------------------------
 # helper functions
@@ -51,14 +33,32 @@ duration_to_hours <- function(duration_str) {
     return(NA)
   }
 }
-#-------------------------------------------------
-# EDA
 
+#-------------------------------------------------
+# import
+psps_circuit <- read_csv(paste0(raw_dir, psps_file_name))
 psps_temp <- psps_circuit %>% 
     dplyr::select(c(Circuit_Name_ICA, PSPS_Event_ID, Sub_Event_ID, Outage_Start, Outage_Full_Restoration,
             Outage_Duration, Total_Customers_Impacted)) %>% 
     mutate(duration = sapply(Outage_Duration, duration_to_hours)) %>% 
     rename_all(tolower)
+
+pge_polyline <- st_read(paste0(raw_dir, "ICA_circuits/PGE/PGE_circuits_lines/PGE_circuits_lines.shp"))
+sce_polyline <- st_read(paste0(raw_dir, "ICA_circuits/SCE/SCE_circuits/SCE_circuits.shp"))
+sdge_polyline <- st_read(paste0(raw_dir, "ICA_circuits/SDGE_circuits/SDGE_circuits.shp"))
+
+households <- read_parquet(paste0(clean_dir, "us_zcta_households.parquet"))
+businesses <- read_parquet(paste0(clean_dir, "us_zcta_cbp.parquet"))
+
+ca_pop_raster <- raster(paste0(raw_raster_dir, "CAPOP_2020_100m_TOTAL.tif"))
+
+zcta_shp <- st_read(paste0(clean_dir, "us_zcta_boundaries.geojson"))
+zcta_shp <- st_transform(zcta_shp, crs(CRS))
+zcta_ca <- read_parquet(paste0(clean_dir, "us_ca_zcta_shp.parquet"))
+zcta_ca <- st_as_sf(zcta_ca, crs = CRS)
+
+#-------------------------------------------------
+# EDA
 
 # histogram of outage_duration
 duration_hist <- ggplot(psps_temp, aes(x = duration)) + 
@@ -112,6 +112,7 @@ psps_hourly <- psps_temp %>%
         customers_out_per_hr = total_customers_impacted/duration) %>%
     ungroup()
 
+
 ## TODO: USE TOTAL RESIDENTIAL TO MATCH DENOM AND BC WE CARE ABOTU RESIDENCES NOT BUSINESSES!
     
 psps_expanded <- psps_hourly %>%
@@ -145,131 +146,112 @@ psps_clean <- num %>%
                 outage_end = as.POSIXct(outage_end,
                     format = "%m/%d/%Y %H:%M",
                     tz = "America/Los_Angeles"),
-                row_start = as.POSIXct(row_start,
-                    format = "%m/%d/%Y %H:%M",
-                    tz = "America/Los_Angeles"),
-                row_end = as.POSIXct(row_end,
-                    format = "%m/%d/%Y %H:%M",
-                    tz = "America/Los_Angeles"))
+                pct_cust_out = total_customers_impacted/pop,
+                duration = as.numeric(duration, units = "hours"),
+                pct_cust_out = ifelse(pct_cust_out > 1, 1, pct_cust_out)) # if more than 100% of the population is out, set to 100%
 
+
+                
+# expand back to hourly dataset
+psps_clean_hourly <- psps_clean %>%
+  rowwise() %>%
+  mutate(hour = list(seq(from = floor_date(outage_start, "hour"),
+                         to = ceiling_date(outage_end, "hour") - hours(1),
+                         by = "hour"))) %>%
+  unnest(hour) %>%
+  ungroup() %>%
+  mutate(hours_since_start = as.numeric(difftime(hour, outage_start, units = "hours"))) %>% 
+  mutate(hours_since_start = ifelse(hours_since_start < 0, 0, hours_since_start))
+
+write.csv(psps_clean_hourly, paste0(clean_dir, "psps_underlying_zcta_clean_hourly.csv"))
 write.csv(psps_clean, paste0(clean_dir, "psps_underlying_zcta_clean.csv"))
-
-
-
-psps_collapse <- psps_clean %>% 
-            group_by(psps_event_id, zcta, row_start) %>%
-            summarise(duration = sum(duration),
-                total_customers_impacted = sum(total_customers_impacted),
-                pop = mean(pop),
-                customers_out_per_hr = total_customers_impacted/duration,
-                pct_cust_out = total_customers_impacted/pop)
-
 
 # step 3: diagnostics
 # top outage events: 
-top_events <- psps_collapse %>% 
-    group_by(psps_event_id) %>% 
-    summarise(total_customers_impacted = sum(total_customers_impacted)) %>% 
+top_events_abs <- psps_clean %>% 
     arrange(desc(total_customers_impacted)) %>% 
+    mutate(hybrid = total_customers_impacted * pct_cust_out,
+        type = 'absolute space') %>% 
     head(10)
-top_event_ids <- top_events$psps_event_id
+top_events_pct <- psps_clean %>% 
+    filter(pct_cust_out ==1) %>%
+    arrange(desc(total_customers_impacted)) %>% 
+    mutate(hybrid = total_customers_impacted * pct_cust_out,
+            type = 'percent space') %>% 
+    head(10)
+top_events_hybrid <- psps_clean %>% 
+    mutate(hybrid = total_customers_impacted * pct_cust_out,
+            type = 'hybrid') %>%
+    arrange(desc(hybrid)) %>%
+    head(10)
 
-top_events_map_df <- psps_collapse %>% 
-    filter(psps_event_id %in% top_event_ids) %>% 
-    group_by(psps_event_id, zcta) %>% 
-    summarise(total_customers_impacted = sum(total_customers_impacted),
-            pop = mean(pop)) %>% 
-    ungroup() %>% 
-    mutate(pct_cust_out = total_customers_impacted/pop) %>% 
-    left_join(zcta_shp, by = c("zcta")) %>% 
-    st_as_sf()
+table_df <- rbind(top_events_abs, top_events_pct, top_events_hybrid)
 
-# plot top events on map
-map <- ggplot() + 
-    geom_sf(data = zcta_ca, fill = "white", color = "gray") + 
-    geom_sf(data = top_events_map_df, aes(fill = total_customers_impacted), color = "black") + 
-    scale_fill_viridis_c() + 
-    labs(title = "Top 10 PSPS Events by ZCTA", 
-        fill = "Total Customers Impacted") + 
-    theme_void()
-map_pct <- ggplot() + 
-    geom_sf(data = zcta_ca, fill = "white", color = "gray") + 
-    geom_sf(data = top_events_map_df %>% filter(pct_cust_out <=100), aes(fill = pct_cust_out), color = "black") + 
-    scale_fill_viridis_c() + 
-    labs(title = "Top 10 PSPS Events by ZCTA", 
-        fill = "Percent Customers Impacted") + 
-    theme_void()
-
-hist <- ggplot(top_events_map_df, aes(x = total_customers_impacted)) + 
-    geom_histogram(bins = 100, fill = "light blue") + 
-    labs(title = "Histogram of Total Customers Impacted (by event-zcta)", 
-        x = "Total Customers Impacted", 
-        y = "Frequency") + 
-    theme_bw()
-
-hist_pct <- ggplot(top_events_map_df %>% filter(pct_cust_out<=100), aes(x = pct_cust_out)) + 
-    geom_histogram(bins = 100, fill = "light blue") + 
-    labs(title = "Histogram of Percent Customers Impacted (by event-zcta)", 
-        x = "Percent Customers Impacted", 
-        y = "Frequency") + 
-    theme_bw()
-
-pdf(paste0(plot_dir, "top_psps_events_plots.pdf"), width = 11, height = 8.5)
-hist + hist_pct
-map + map_pct
-dev.off()
-
-# outstanding issues 
-# how to collapse
-    # what to do about rows with more than 100% out due to sub event handlign during collapse 
-# how to split rows that span multiple days
+write.csv(table_df, paste0(plot_dir, "top_psps_events.csv"))
 
 
 
+# # step 3: aggregate the by day and ZCTA:
+
+# # A. summarize the data by ZCTA and day
+# # create variables event_in_last_week and event_in_last_month.
+# # filter out the rows where event_in_last_week is 0 and event_in_last_month is 1.
+
+# # agg data by day and ZCTA. sum total customers but average duration
+# psps_daily <- psps_clean %>%
+#     mutate(day = as.Date(row_start)) %>%
+#     group_by(zcta, day) %>%
+#     summarise(
+#         total_customers_impacted_day = sum(total_customers_impacted),
+#         duration_day = mean(duration),
+#         pop = first(pop)
+#     )
+
+# # create event_in_last_week and event_in_last_month variables
+# psps_daily <- psps_daily %>%
+#     arrange(zcta, day) %>%
+#     group_by(zcta) %>%
+#     mutate(
+#         event_in_last_week = lag(cumsum(!is.na(day)), 7, order_by = day) > 0,
+#         event_in_last_month = lag(cumsum(!is.na(day)), 30, order_by = day) > 0
+#     )
+
+# # filter out rows where there was an event in the last month but not the last week
+# psps_daily_filtered <- psps_daily %>%
+#     filter(!(event_in_last_week != TRUE & event_in_last_month == TRUE))
+
+# # B. plot
+# # identify the top 10 ZCTA-days based on total_customers_impacted and duration.
+# top_10_zcta_days_cust <- psps_daily_filtered %>%
+#     arrange(desc(total_customers_impacted_day)) %>%
+#     head(10) %>% 
+#     merge(zcta_ca, by = "zcta", all.x = TRUE) %>% 
+#     st_as_sf()
+# top_10_zcta_days_dur <- psps_daily_filtered %>%
+#     arrange(desc(duration_day)) %>%
+#     head(10) %>% 
+#     merge(zcta_ca, by = "zcta", all.x = TRUE) %>% 
+#     st_as_sf()
+
+# # create a map with CA counties as the basemap and the selected ZCTAs filled in based on the variables total_customers_impacted and duration
+# map_customers <- ggplot() + 
+#     geom_sf(data = zcta_ca, fill = "white", color = "gray") + 
+#     geom_sf(data = top_10_zcta_days_cust, aes(fill = total_customers_impacted_day), color = "black") + 
+#     scale_fill_viridis_c() + 
+#     labs(title = "Top 10 PSPS events by by ZCTA-day: customers impacted", 
+#         fill = "customers impacted") + 
+#     theme_void()
+# map_duration <- ggplot() + 
+#     geom_sf(data = zcta_ca, fill = "white", color = "gray") + 
+#     geom_sf(data = top_10_zcta_days_dur, aes(fill = duration_day), color = "black") + 
+#     scale_fill_viridis_c() + 
+#     labs(title = "Top 10 PSPS events by by ZCTA-day: duration", 
+#         fill = "duration") + 
+#     theme_void()
+
+# map_customers + map_duration
 
 
 
 
-
-
-
-
-
-
-# step 3: estimate the number of hours in every 24 hour period (day) where {x}% or more of the customers were without power.
-    # we will use a data driven threshold (25%, 50%, etc.)
-    # look at the distribution of percents and pick a reasonable cutpoint.
-    # count up # of hours that have more than that % off
-    # if we want a binary cut for a synthetic control, that can be based on the distribution
-# thresholds <- c(0.25, 0.5, 0.75)
-# threshold <- 0.25
-# # for(threshold in thresholds){
-# psps_clean <- psps_clean %>% 
-#     mutate(row_hr = ((as.numeric(row_end) - as.numeric(row_start))/60/60), # this is to account for rows that are less than 1 hr at the end of an event
-#         customers_out_per_hr = total_customers_impacted/row_hr,
-#         pct_cust_out = customers_out_per_hr/pop,
-#         pct_out_over_thresh = ifelse(pct_cust_out > threshold, 1, 0),
-#         day = as_date(row_start))  # need to figure out how to split a row that spans 2 days 
-
-# # calculate the number of hours in psps_event_id - day where {x}% or more of the customers were without power
-# psps_collapse_by_day <- psps_clean %>% 
-#     group_by(psps_event_id, day, zcta, row_start) %>%
-#     summarise(duration = max(duration), 
-#             total_customers_impacted = max(total_customers_impacted),
-#             pct_out_over_thresh = max(pct_out_over_thresh)) %>% 
-#     unique() %>% 
-#     group_by(psps_event_id, day, zcta) %>% 
-#     summarise(hrs_with_high_cust_out = sum(pct_out_over_thresh)) %>% 
-#     ungroup()
-
-
-# create histogram of pct_out value with vertical line at x% threshold using ggplot
-# ggplot(psps_collapse_by_day, aes(x = hrs_with_high_cust_out)) + 
-#     geom_histogram(bins = 100, fill = "light blue") + 
-#     labs(title = paste("Histogram of hours per day with more than ", threshold*100, "% of customers out"), 
-#         x = paste0("Hours per day with more than ", threshold*100, "% of customers out"), 
-#         y = "Frequency") + 
-#     # geom_vline(xintercept = threshold*100, color = "red", size = 2) + 
-#     theme_bw()
-# }
 
